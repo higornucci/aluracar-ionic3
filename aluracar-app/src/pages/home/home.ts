54621626223fab41d03e8d6a32051e7c52c82c23
Higor Nucci <higornucci@gmail.com>
2018-04-10 13:20:31 -0400

    serviço externo para buscar carros

--- aluracar-app/src/pages/home/home.ts
+++ aluracar-app/src/pages/home/home.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Carro } from "../../models/carro";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public carros;
+  public carros: Carro[];
 
-  constructor(public navCtrl: NavController) {
-    this.carros = [
-        {
-          nome: 'Brasília Amarela',
-          preco: 2000
-        }
-    ];
+  constructor(public navCtrl: NavController, private _http: HttpClient) {
+    this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
+        .subscribe(
+            (carros) => {
+              this.carros = carros;
+            }
+        )
   }
 
 }
